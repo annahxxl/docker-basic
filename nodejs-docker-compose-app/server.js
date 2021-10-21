8c08330b64773f44cc280c63312121c1a65f68ca
Hanna Lee <8annahxxl@gmail.com>
2021-10-21 20:12:16 +0900

    add: Node.js 앱 생성

---
+++ nodejs-docker-compose-app/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const redis = require("redis");
+
+// 레디스 클라이언트 생성
+const client = redis.createClient({
+  host: "redis-server",
+  port: 6379,
+});
+
+const app = express();
+
+// 숫자는 0부터 시작합니다.
+client.set("number", 0);
+
+app.get("/", (req, res) => {
+  client.get("number", (err, number) => {
+    // 현재 숫자를 가져온 후에 1씩 올려줍니다.
+    client.set("number", parseInt(number) + 1);
+    res.send("숫자가 1씩 올라갑니다. 현재 숫자: " + number);
+  });
+});
+
+app.listen(8080, () => {
+  console.log("🚀 Server is running ...");
+});
